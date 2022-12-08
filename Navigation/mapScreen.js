a88fb2432fbe172d6a4b95aa8a21c23469b72039
Rayna3 <raynabhattacharyya@gmail.com>
2022-12-08 12:18:02 -0800

    pictures scroll :)

--- Navigation/mapScreen.js
+++
@@ -1,9 +0,0 @@
-export default function mapScreen({navigation}) {
-    return(
-        <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text
-                onPress={()=> navigation.navigate('')}
-                style={{fontSize: 26, fontWeight: 'bold'}}> Home Screen</Text>
-        </View>
-    );
-}
--- Navigation/screens/TestMarker.js
+++ Navigation/screens/TestMarker.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
-import { View, Text, Image} from 'react-native';
+import { View, Text, Image, ScrollView, StyleSheet} from 'react-native';
 
 
 export default function MarkerScreen({ navigation }) {
     return (
         <View style={{ dlex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#efe4be" }}>
-            <View>
-                <Image source={require("../images/Marker_1_1.jpg")} style={{ height: 300, width: 400, top: 0, resizeMode: "resize"}}></Image>
-            </View>
+            <ScrollView bounces="false">
+                <View style={{flexDirection: "row"}}>
+                    <Image source={require("../images/Marker_1_1.jpg")} style={{ height: 300, width: 400, top: 0, resizeMode: "resize", borderW: 10}}></Image>
+                    <Image source={require("../images/Marker_1_2.jpg")} style={{ height: 300, width: 400, top: 0, resizeMode: "resize"}}></Image>
+                </View>
+            </ScrollView>
             <View style={{ borderColor: "#efe4be", borderWidth: 5, borderRadius: 15, marginTop: -35, backgroundColor: "#90C6CA", width: 180}}>
                 <Text style={{ padding: 10, fontFamily: "Times New Roman", fontSize: '30', alignSelf: "center", color: "white" }}>Marker 1</Text>
             </View>
