78a0dfca0960d86fd61eab06b2ea71209ea69275
Rayna <rayna_b@berkeley.edu>
2023-02-22 17:18:58 -0800

    pop-up button works

--- Navigation/screens/Marker.js
+++ Navigation/screens/Marker.js
@@ -1,7 +1,8 @@
-import * as React from 'react';
-import { View, Text, Image, FlatList, StyleSheet, ScrollView, Button} from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Image, FlatList, StyleSheet, ScrollView, Button, Modal} from 'react-native';
 
 export default function Marker(props) {
+    const [popUp, setPopUp] = useState(false);
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#efe4be" }}>
             <FlatList bounces="false"
@@ -25,11 +26,41 @@ export default function Marker(props) {
             <ScrollView>{props.text}</ScrollView>
             <View style={{ borderColor: "#efe4be", borderWidth: 1, borderRadius: 15, backgroundColor: "#90C6CA", width: 150, marginBottom: 10, marginTop: 10, shadowOpacity: 0.3, shadowRadius: 10,}}>
                 <Button
+                    onPress={() => {
+                        setPopUp(!popUp)
+                    }}
                     title="Learn More"
                     color="black"
                     accessibilityLabel="Learn more about this purple button"
                 />
             </View>
+            <Modal transparent={true} visible={popUp}>
+                <View style={{backgroundColor: "#000000aa", flex: 1, alignContent: 'center'}}>
+                    <View style={{backgroundColor: "#efe4be", margin: 50, marginTop: 250, padding: 40, borderRadius: 10, alignSelf: 'center'}}>
+                        <Text style={{fontSize: 80, color: "red"}}>hiiiiiiiiiii</Text>
+                        <View style={{ borderColor: "#efe4be", borderWidth: 1, borderRadius: 15, backgroundColor: "#90C6CA", width: 150, marginBottom: 10, marginTop: 10, shadowOpacity: 0.3, shadowRadius: 10,}}>
+                            <Button
+                                onPress={() => {
+                                    setPopUp(!popUp)
+                                }}
+                                title="Back"
+                                color="black"
+                                accessibilityLabel="Learn more about this purple button"
+                            />
+                        </View>
+                        <View style={{ borderColor: "#efe4be", borderWidth: 1, borderRadius: 15, backgroundColor: "#90C6CA", width: 150, marginBottom: 10, marginTop: 10, shadowOpacity: 0.3, shadowRadius: 10,}}>
+                            <Button
+                                onPress={() => {
+                                    setPopUp(!popUp)
+                                }}
+                                title="Next Marker"
+                                color="black"
+                                accessibilityLabel="Learn more about this purple button"
+                            />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 }
