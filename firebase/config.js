eeb86d051a60dc8fa218de3de02b616f6ccf26a9
Chetan Goenka <cgoenka@berkeley.edu>
2023-10-20 20:27:09 -0700

    added firebase setup

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQcFezJBuaV2MUn3jX9SSpuGKNoCdYyRw",
+  authDomain: "ruth-bancroft-garden.firebaseapp.com",
+  projectId: "ruth-bancroft-garden",
+  storageBucket: "ruth-bancroft-garden.appspot.com",
+  messagingSenderId: "976432634857",
+  appId: "1:976432634857:web:d73d2cc9c5e118446dc978",
+  databaseURL: 'https://ruth-bancroft-garden.firebaseio.com'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
