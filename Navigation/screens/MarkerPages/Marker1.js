9cf2bf48b99e7555d369551759c73e66810d3f8a
Angela He <angela.he@berkeley.edu>
2024-01-01 23:06:55 -0800

    started connecting garden app to fetch marker data from firebase

--- Navigation/screens/MarkerPages/Marker1.js
+++ Navigation/screens/MarkerPages/Marker1.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import { View, Text, Image, FlatList, StyleSheet} from 'react-native';
 import Marker from '../Marker';
+import { db } from '../../../Firebase/firebase'
+import { ref, onValue } from 'firebase/database'
 
 Text.defaultProps = {}
 Text.defaultProps.maxFontSizeMultiplier = 2.
+
+const markerRef = ref(db, 'plants/Marker1');
+var data;
+onValue(markerRef, (snapshot) => {
+    data = snapshot.val() || {};
+    console.log(data);
+});
 
 const images = [
     require('../../images/Marker_1_1.jpg'),
@@ -20,17 +29,27 @@ const styles = {
 
 const text = 
     <Text style={{ padding: 20, fontFamily: "Times New Roman", fontSize: 20 }}>   
-        This <Text style={{fontWeight: "bold"}}>250-year-old Valley Oak (<Text style={{fontStyle: "italic"}}>Quercus lobata</Text>)</Text> is a reminder that <Text style={{fontWeight: "bold"}}>this landscape was once oak woodland, the land of the Bay Miwoks.</Text>
-        {"\n"} {"\n"}
-        In front of the oak is the enormous <Text style={styles.text}>Agave franzosinni</Text>.
+    {data.description}
     </Text>
+
+// const text = 
+//     <Text style={{ padding: 20, fontFamily: "Times New Roman", fontSize: 20 }}>   
+//         This <Text style={{fontWeight: "bold"}}>250-year-old Valley Oak (<Text style={{fontStyle: "italic"}}>Quercus lobata</Text>)</Text> is a reminder that <Text style={{fontWeight: "bold"}}>this landscape was once oak woodland, the land of the Bay Miwoks.</Text>
+//         {"\n"} {"\n"}
+//         In front of the oak is the enormous <Text style={styles.text}>Agave franzosinni</Text>.
+//     </Text>
 
 const popUpText = 
     <Text style={{padding: 15, fontFamily: "Times New Roman", fontSize: 20 }}>
-        Agaves are distinguished by their rosettes of tough leaves, often with a spiny margin terminating in a sharp point. 
-        {"\n"} {"\n"}
-        <Text style={{fontStyle:"italic"}}>Look for Agaves in various shapes and sizes throughout the Garden.</Text>
+    {data.popUpText}
     </Text>
+
+// const popUpText = 
+//     <Text style={{padding: 15, fontFamily: "Times New Roman", fontSize: 20 }}>
+//         Agaves are distinguished by their rosettes of tough leaves, often with a spiny margin terminating in a sharp point. 
+//         {"\n"} {"\n"}
+//         <Text style={{fontStyle:"italic"}}>Look for Agaves in various shapes and sizes throughout the Garden.</Text>
+//     </Text>
 
 const map = require('../../images/map/1.jpg')
 
